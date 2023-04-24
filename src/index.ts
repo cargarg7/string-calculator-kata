cd88ad7b2582cc23ff0c9702c2dcb6389b4c81f5
cargarg7 <cargarg7@gmail.com>
2023-04-24 15:51:59 +0200

    Refactor to object

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
 const RADIX = 10;
+const BEGIN_SPLIT_SEPARATOR = '//';
 const SPLIT_SEPARATOR = '/';
 const DEFAULT_SEPARATOR = ',';
 const regex = /(?!^\d+$)^.+$/;
 
-/**
- *    "//%/1%2%3"
- *      Separator => %
- *      Numbers => 1%2%3
- **/
-function getValues(stringOfValues) {
-	const valuesWithCustomSeparator = stringOfValues.split(SPLIT_SEPARATOR);
-	const separator = valuesWithCustomSeparator.length > 2 ? valuesWithCustomSeparator[2] : DEFAULT_SEPARATOR;
-	const valuesFormatted =
-		valuesWithCustomSeparator.length > 2 ? valuesWithCustomSeparator[3] : valuesWithCustomSeparator[0];
-	return valuesFormatted.split(separator);
+const VALUES_BY_SEPARATOR = {
+	DEFAULT_SEPARATOR: (stringOfValues: string): string[] => stringOfValues.split(DEFAULT_SEPARATOR),
+	CUSTOM_SEPARATOR: (stringOfValues: string): string[] => {
+		const separator = stringOfValues.slice(BEGIN_SPLIT_SEPARATOR.length, stringOfValues.lastIndexOf(SPLIT_SEPARATOR));
+		return stringOfValues.slice(stringOfValues.lastIndexOf(SPLIT_SEPARATOR) + 1).split(separator);
+	},
+};
+
+function isCustomSeparator(stringOfValues: string): boolean {
+	return stringOfValues.startsWith(BEGIN_SPLIT_SEPARATOR);
 }
 
 export function stringCalculator(stringOfValues?: string): number {
 	if (!stringOfValues) return 0;
 
-	const values = getValues(stringOfValues);
-	return values.reduce(
+	const getValuesFn = isCustomSeparator(stringOfValues)
+		? VALUES_BY_SEPARATOR.CUSTOM_SEPARATOR
+		: VALUES_BY_SEPARATOR.DEFAULT_SEPARATOR;
+	return getValuesFn(stringOfValues).reduce(
 		(acc: number, value: string) => (acc += regex.test(value) ? 0 : Number.parseInt(value, RADIX)),
 		0
 	);
